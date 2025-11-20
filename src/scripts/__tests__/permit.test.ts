f895acd31d9e80b7dca280ce4761c2c68ee54e53
grotesq-ue <ulysesadrales@gmail.com>
2025-11-20 19:06:08 +0800

    test: utils, scripts
    ✅ update tests for pmd utility function
    ✅ update tests for permit script function

--- src/scripts/__tests__/permit.test.ts
+++ src/scripts/__tests__/permit.test.ts
@@ -1,19 +1,19 @@
 import { afterEach, describe, expect, mock, spyOn, test } from "bun:test";
 import { permit } from "../permit";
 
-const prompt = spyOn(global, "prompt");
-
 describe("permit", () => {
   afterEach(() => mock.clearAllMocks());
 
   test("returns true if the user accepts", () => {
-    prompt.mockReturnValueOnce(null);
+    const prompt = spyOn(global, "prompt");
+    prompt.mockImplementationOnce(() => null);
     const permitted = permit();
     expect(permitted).toBeTrue();
   });
 
   test("returns false if the user rejects", () => {
-    prompt.mockReturnValueOnce("no");
+    const prompt = spyOn(global, "prompt");
+    prompt.mockImplementationOnce(() => "no");
     const permitted = permit();
     expect(permitted).toBeFalse();
   });
--- src/utils/__tests__/pmd.test.ts
+++ src/utils/__tests__/pmd.test.ts
@@ -2,29 +2,30 @@ import { afterEach, describe, expect, mock, spyOn, test } from "bun:test";
 import * as PMD from "package-manager-detector";
 import { pmd } from "../pmd";
 
-const detect = spyOn(PMD, "detect");
-const resolveCommand = spyOn(PMD, "resolveCommand");
-
 const PARAMS: Parameters<typeof pmd> = ["execute", "command"];
 
 describe("pmd", () => {
   afterEach(() => mock.clearAllMocks());
 
   test("returns a command string", async () => {
-    detect.mockResolvedValueOnce({ name: "npm", agent: "npm" });
+    const detect = spyOn(PMD, "detect");
+    detect.mockImplementationOnce(async () => ({ name: "npm", agent: "npm" }));
     const command = await pmd(...PARAMS);
     expect(command).toBe("npx command");
   });
 
   test("returns undefined when package manager is undetected", async () => {
-    detect.mockResolvedValueOnce(null);
+    const detect = spyOn(PMD, "detect");
+    detect.mockImplementationOnce(async () => null);
     const command = await pmd(...PARAMS);
     expect(command).toBe(undefined);
   });
 
   test("returns undefined when command is unresolved", async () => {
-    detect.mockResolvedValueOnce({ name: "npm", agent: "npm" });
-    resolveCommand.mockReturnValueOnce(null);
+    const detect = spyOn(PMD, "detect");
+    const resolveCommand = spyOn(PMD, "resolveCommand");
+    detect.mockImplementationOnce(async () => ({ name: "npm", agent: "npm" }));
+    resolveCommand.mockImplementationOnce(() => null);
     const command = await pmd(...PARAMS);
     expect(command).toBe(undefined);
   });

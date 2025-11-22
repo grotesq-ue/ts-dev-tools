e2a005ed59f50f1d1e739f752f2c74cf1571b6e9
grotesq-ue <ulysesadrales@gmail.com>
2025-11-22 15:42:34 +0800

    chore: build configuration
    🔧 Remove unnecessary file copies

--- bunup.config.ts
+++ bunup.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from "bunup";
-import { copy } from "bunup/plugins";
 
 export default defineConfig({
   shims: true,
   unused: true,
   minify: true,
-  plugins: [
-    copy(["README.md", "LICENSE.md"]),
-  ],
 });

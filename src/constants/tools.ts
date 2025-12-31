a1a170b1d61f83c59df0002c16f4d9832af8cddb
grotesq-ue <ulysesadrales@gmail.com>
2025-12-31 22:48:02 +0800

    refactor: git hook setup
    ♻️ Allow no file input for formatting job

--- src/constants/tools.ts
+++ src/constants/tools.ts
@@ -4,7 +4,7 @@ export type Dep = typeof DEPS[number];
 
 export const JOBS = new Map([
   ["oxlint", { run: "oxlint --fix {staged_files}", stage_fixed: true }],
-  ["dprint", { run: "dprint fmt {staged_files}", stage_fixed: true }],
+  ["dprint", { run: "dprint fmt --allow-no-files {staged_files}", stage_fixed: true }],
 ]) satisfies Map<Dep, object>;
 
 export const LEFTHOOK_CONFIG_PATH = "./lefthook.yml";
